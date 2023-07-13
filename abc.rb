409c84a006977a9b94c42f29a5a18c1574abf6b3
Shubhank-Kushwaha <shkuhsjik@gmail.com>
2023-07-13 15:19:55 +0530

    new file

---
+++ abc.rb
@@ -0,0 +1,262 @@
+# puts "shubhank"
+
+    # def add_three(number)
+    #     return number + 3
+    #   end
+      
+    #   returned_value = add_three(4)
+    #   puts returned_value  
+#     def example(n)
+#         n+2
+#     end
+#     example(3).times{puts "this should print 5times"}
+# p "shubhank".length.to_i
+
+# def exam(n)
+#    puts n+3
+# end 
+
+# def add_three(n)
+#     new_value = n + 3
+#     puts new_value
+#     new_value
+#   end
+# def greeting(name)
+#     puts "hello "+ name+ " how are you?"
+# end
+# greeting("bob")
+#  def add(val1, val2)
+#    c= val1+val2
+#  end
+#  add(5,6)
+#  def scream(words)
+#     words = words + "!!!!"
+#     puts words
+
+#     return
+#   end
+  
+# #   scream("Yippeee")
+#------------------------------------------------
+# def ab(name1)
+#     if name1.length>10
+#         puts name1.upcase
+#     else
+#         puts name
+#     end end
+#      ab("shubhank kushwaha")
+
+# def exam(n)
+#     n+3
+# end
+#    exam(4).times{puts "this is 7 times"}  
+# def example(n)
+#              n+2
+#        end
+# #          example(3).times{puts "this should print 5times"}# Snippet 3
+# y = 9
+# x = 10
+# if (x + 1) <= (y)
+#   puts "Alright."
+# elsif (x + 1) >= (y)
+#   puts "Alright now!"
+# elsif (y + 1) == x
+#   puts "ALRIGHT NOW!"
+# else
+#   puts "Alrighty!" 
+# end
+
+
+# ----------------LOOP----------------
+
+
+# loop do 
+#     puts "hey here loops starting"
+# end
+
+# i=0
+# loop do 
+# i+=1
+# puts i
+# break 
+# end
+# # puts i
+# i=0
+# loop do 
+#     i+=2
+#     puts i 
+# if i ==10
+#     break 
+# end
+# end
+
+# x=gets.chomp.to_i
+# while x>=0
+#     puts x
+#     x+=1
+    
+     
+#     break
+#     next
+# end
+#  puts "done!"
+                   
+
+# x= gets.chomp.to_i
+# until (x<=0)                   # opposite of while loop
+#     puts x
+#     x+=1
+#     break
+# end
+#  puts "done!"
+ 
+# begin
+#     puts "Do you want to do that again?"
+#     answer = gets.chomp
+#   end while answer == 'Y'
+
+
+# x =gets.chomp.to_i
+# for i in 1..x do
+#     puts x-i 
+#     end 
+#     puts "Done!"
+
+
+# x= [1,2,3,4,5]
+# for i in x.reverse do 
+#     puts i  
+# end
+# puts "Done!"
+
+# x=0;
+#  while (x<=10)
+#     if x.odd?
+#         puts x
+#     end
+#         x+=1
+    
+# end
+# puts "Done!"
+
+
+# x=0
+# while(x<=10)
+#     if(x==3)
+#         x+=1
+#         next
+#     elsif x.odd?
+#         puts x
+#     end
+#     x+=1
+# end
+# puts "Done!"
+
+# -----------------------------ItERATOR------------------------------
+
+# names= ["paritosh","Shubhank","Bhavna","Palak"]
+# # names.each{|names| puts names}
+
+# puts names --same work as up
+
+# names= ["paritosh","Shubhank","Bhavna","Palak"]
+# x=1
+# names.each do |names|
+    
+#     puts "#{x}.#{names}"
+#     x+=1
+# end
+# $a =10
+# class Abc
+#     @@class_v =9
+#     def instance()
+#         a = 0
+#         puts "i am instance#{@@class_v} #{a}"
+#         @instance_v =5
+#     end
+#      def initil ()
+#         puts "i am initialize#{@instance_v}"
+#         a= 10 #local var 
+#      end
+#     end
+# obj= Abc.new
+# obj.instance 
+# obj.initil
+
+     
+# def fibonacci(number)
+#     if number < 2
+#       number
+#     else
+#       fibonacci(number - 1) + fibonacci(number - 2)
+#     end
+#   end
+  
+#   puts fibonacci(6)
+
+
+# def sum(a, b = 5)
+#     a + b
+# end
+
+
+# p sum(10)
+
+
+# a = gets.chomp.to_i
+# b = gets.chomp.to_i
+# if(a > b)
+#     puts "a is greater=#{a}"
+# else
+#     puts "b is greater=#{b}"
+# end
+# a > b ? puts "a is greater #{a}": puts"b is greater #{b}"
+
+
+# ar= [3, 4, 7, 2, 1]
+# mx=0
+# mx2=0
+# for i in ar
+#     if(mx<i)
+#         mx= i
+        
+#     end
+#     if(mx2>i &&mx2<mx)
+#         mx2=i
+#     end
+    
+# end    
+# print mx2
+
+
+# for i in 1...5
+#     puts i
+# end
+
+
+# x = 4
+# until x == 10
+#     puts "geeks"
+#    x += 1
+# end
+# a= 0
+# loop do
+#     a += 1
+#     p 
+#     break if a > 5
+# #  end
+# arr = [1,3,4,16, 7]
+# def array_print(arr)
+#     arr.each do |num| 
+#         puts num
+#     end.max
+# end
+
+# print array_print(arr)
+
+# # 1
+# # 3
+# # 4
+# # 6
+# # nil
+# arr=["bhavana" , "Shubhank" , "Palak" , "Paritosh"]
