327f53ae497f4a54875e853327ffe8bc5acb685d
Shubhank-Kushwaha <shkuhsjik@gmail.com>
2023-07-17 10:09:41 +0530

    More-Stuff

--- More-Stuff.rb
+++ More-Stuff.rb
@@ -192,17 +192,17 @@
 # end
 # top
 
-def space_out_letters(person)
-    return person.split("").join(" ")
-  end
+# def space_out_letters(person)
+#     return person.split("").join(" ")
+#   end
   
-  def greet(person)
-    return "H e l l o, " + space_out_letters(person)
-  end
+#   def greet(person)
+#     return "H e l l o, " + space_out_letters(person)
+#   end
   
-  def decorate_greeting(person)
-    puts greet(person) 
-  end
+#   def decorate_greeting(person)
+#     puts greet(person) 
+#   end
   
-  decorate_greeting("John")
-  decorate_greeting("Shubh")
+#   decorate_greeting("John")
+#   decorate_greeting("Shubh")
